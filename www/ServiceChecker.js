8bc6aed44467d17d8f79fd1acca50d22dd6e8d7f
xiaoaowanghu <xiaoaowanghu@sina.com>
2017-11-17 14:35:06 +0800

    Update ServiceChecker.js

--- www/ServiceChecker.js
+++ www/ServiceChecker.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
+exports.check = function (arg0, success, error) {
     exec(success, error, 'ServiceChecker', 'checkGooglePlayService', []);
 };
